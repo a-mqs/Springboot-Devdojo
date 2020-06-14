e9f97a69ad88a741652057f351885e360690a2e4
Amanda <amporto10@gmail.com>
2020-06-14 20:39:41 -0300

    Métodos GET e POST, CustomErrorType e att Student

--- src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -1,11 +1,12 @@
 package br.com.devdojo.endpoint;
 
+import br.com.devdojo.error.CustomErrorType;
 import br.com.devdojo.model.Student;
 import br.com.devdojo.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,9 +14,9 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 /**
- * ? Essa é a classe que define o ponto final de
- * ? até onde a API é acessada, nela contém os principais
- * ? métodos.
+ * Classe que define o ponto final até onde essa aplicação
+ * (ou uma outra API) é acessada, nela contém os principais
+ * métodos HTTP para acesso dos recursos.
  */
 
 @RestController
@@ -25,13 +26,29 @@ public class StudentEndpoint {
     private DateUtil dateUtil;
 
     @RequestMapping(method = RequestMethod.GET, path = "/list")
-    public List<Student> listAll(){
+    public ResponseEntity<?> listAll(){
         System.out.println("Data e hora da request: " + dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        List<Student> studentList = asList(new Student("Kim Namjoon"),
-                new Student("Kim Seokjin")
-        );
-//        System.out.println("Data e hora da response: " + dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return studentList;
+        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
     }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable("id") int id){
+        Student student = new Student();
+        student.setId(id);
+        int index = Student.studentList.indexOf(student);
+        if (index == -1)
+            return new ResponseEntity<>(new CustomErrorType("Student not found."), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student){
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+
+
+
 
 }
---
+++ src/main/java/br/com/devdojo/error/CustomErrorType.java
@@ -0,0 +1,18 @@
+package br.com.devdojo.error;
+
+/**
+ * Classe usada para a descrição de erros nos retornos
+ * dos métodos do endpoint. Geralmente acompanha um
+ * status code.
+ */
+public class CustomErrorType {
+    private String errorMessage;
+
+    public CustomErrorType(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
