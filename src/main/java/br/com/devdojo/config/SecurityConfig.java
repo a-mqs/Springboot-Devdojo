bdc47232543f149ac20cdbf782ef8f8dfe42802b
Amanda <amporto10@gmail.com>
2020-06-24 18:30:23 -0300

    Autenticação e Autorização com Spring Data

--- src/main/java/br/com/devdojo/config/SecurityConfig.java
+++ src/main/java/br/com/devdojo/config/SecurityConfig.java
@@ -1,17 +1,24 @@
 package br.com.devdojo.config;
 
+import br.com.devdojo.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // TODO: 22/06/2020 Adicionar as modificações daqui tudo no github
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -22,12 +29,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser("seonghwa").password(encoder.encode("devdojo")).roles("USER")
-                .and()
-                .withUser("hongjoong").password(encoder.encode("devdojo")).roles("USER", "ADMIN");
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
+
+    //      Configuração de autenticação em memória
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser("seonghwa").password(encoder.encode("devdojo")).roles("USER")
+//                .and()
+//                .withUser("hongjoong").password(encoder.encode("devdojo")).roles("USER", "ADMIN");
+//    }
 }
